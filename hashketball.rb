2c5a73337ce419e7701b132566cce3f10671ac5e
Amanda King <amapickell@gmail.com>
2019-05-30 22:27:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,7 +121,7 @@ def num_points_scored(name)
   hash = game_hash
   hash.each do |location, info| 
       info.each do |attribute, stuff| 
-        if stuff.keys.include?(name) 
+        if stuff.include?(name) 
           return hash[location][attribute][name][:points]
       end
     end
